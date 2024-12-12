18cdd2a62135461883cee86397c694c36d42d356
Dmitriy <dm.tsiganov@gmail.com>
2024-12-12 10:55:03 +0700

    test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера карты"""
+    if len(str(card_number)) != 16:
+        raise ValueError("Неправильный номер карты")
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция маскировки номера счета"""
+    if len(str(account_number)) != 20:
+        raise ValueError("Неправильный номер счета")
     return f"**{account_number[-4:]}"
 
 
