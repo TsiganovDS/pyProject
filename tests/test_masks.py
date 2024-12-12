b05595299c3fe77f4721f2a5449115ce1984f707
Dmitriy <dm.tsiganov@gmail.com>
2024-12-12 15:10:39 +0700

    Финал

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,29 +3,29 @@ import pytest
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_mask_card_number():
+def test_mask_card_number() -> None:
     assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
 
-def test_mask_account():
+def test_mask_account() -> None:
     assert get_mask_account("73654108430135874305") == "**4305"
 
 
-def test_mask_card_no_number_invalid():
+def test_mask_card_no_number_invalid() -> None:
     with pytest.raises(ValueError):
         get_mask_card_number("")
 
 
-def test_mask_card_number_invalid():
+def test_mask_card_number_invalid() -> None:
     with pytest.raises(ValueError):
         get_mask_card_number("7000792289606361098776")
 
 
-def test_mask_account_no_number_invalid():
+def test_mask_account_no_number_invalid() -> None:
     with pytest.raises(ValueError):
         get_mask_account("")
 
 
-def test_mask_account_number_invalid():
+def test_mask_account_number_invalid() -> None:
     with pytest.raises(ValueError):
         get_mask_account("70007922896063618758432098776")
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -4,64 +4,95 @@ from src.processing import filter_by_state, sort_by_date
 
 
 @pytest.fixture
-def filter_fixture():
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+def filter_fixture() -> list:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
 @pytest.fixture
-def filter_fixture_no_state():
-    return [{'id': 41428829, 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}]
+def filter_fixture_no_state() -> list:
+    """fixture"""
+    return [
+        {"id": 41428829, "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
 @pytest.fixture
-def sort_the_same_dates():
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}]
+def sort_the_same_dates() -> list:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 594226727, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+    ]
 
 
-@pytest.mark.parametrize('state, expected', [
-    ('EXECUTED', [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
-    ('CANCELED', [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])])
-def test_filter_by_state(filter_fixture, state, expected):
+@pytest.mark.parametrize(
+    "state, expected",
+    [
+        (
+            "EXECUTED",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            "CANCELED",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+    ],
+)
+def test_filter_by_state(filter_fixture: dict, state: str, expected: str) -> None:
     assert filter_by_state(filter_fixture, state) == expected
 
 
-@pytest.mark.parametrize('keys, expected', [(True, [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
-                                            (False, [{'id': 939719570, 'state': 'EXECUTED',
-                                                      'date': '2018-06-30T02:08:58.425572'},
-                                                     {'id': 594226727, 'state': 'CANCELED',
-                                                      'date': '2018-09-12T21:27:25.241689'},
-                                                     {'id': 615064591, 'state': 'CANCELED',
-                                                      'date': '2018-10-14T08:21:33.419441'},
-                                                     {'id': 41428829, 'state': 'EXECUTED',
-                                                      'date': '2019-07-03T18:35:29.512364'}])
-                                            ])
-def test_sort_by_date(filter_fixture, keys, expected):
+@pytest.mark.parametrize(
+    "keys, expected",
+    [
+        (
+            True,
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            False,
+            [
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            ],
+        ),
+    ],
+)
+def test_sort_by_date(filter_fixture: dict, keys: bool, expected: str) -> None:
     assert sort_by_date(filter_fixture, keys) == expected
 
 
-def test_filter_by_state_invalid_state(filter_fixture_no_state):
+def test_filter_by_state_invalid_state(filter_fixture_no_state: dict) -> None:
     with pytest.raises(ValueError):
         filter_by_state(filter_fixture_no_state)
 
 
-def test_sort_by_date_the_same_dates(sort_the_same_dates):
+def test_sort_by_date_the_same_dates(sort_the_same_dates: list) -> None:
     assert sort_by_date(sort_the_same_dates) == [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}]
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 594226727, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+    ]
