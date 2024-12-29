125c3c46a90ede6db65a167830f9069148fadbf7
Dmitriy <dm.tsiganov@gmail.com>
2024-12-29 23:15:59 +0700

    exter external_api

---
+++ src/external_api.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def tranzaction() -> float:
+    url = "https://api.apilayer.com/exchangerates_data/convert"
+    payload = {"amount": 1, "from": "EUR", "to": "RUB"}
+    headers = {"apikey": "O1j67DhxMMtuEIKXzSryYPNhlgD4aT64"}
+    response = requests.get(url, headers=headers, params=payload)
+    if response.status_code == 200:
+        data = response.json()
+        return data["result"]
+    else:
+        print(f"Ошибка: {response.status_code}")
+        return 0.0
+
+
+print(tranzaction())
