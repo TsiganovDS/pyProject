8f36a79cf050179b4d1809fd41185c647c513dfe
Dmitriy <dm.tsiganov@gmail.com>
2025-01-04 15:21:00 +0700

    Логирование

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,46 @@
+import logging
+import os
+
+path = os.path.join(os.path.dirname(__file__), "..", "logs", "masks.log")
+
+
+def setup_masks_logger() -> logging.Logger:
+    masks_logger = logging.getLogger("masks")
+    masks_logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler(path, mode="w", encoding="utf-8")
+    file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(file_formatter)
+    masks_logger.addHandler(file_handler)
+    return masks_logger
+
+
+masks_logger = setup_masks_logger()
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера карты"""
+    masks_logger.info("Запуск функции get_mask_card_number")
     if len(str(card_number)) != 16:
+        masks_logger.error("Неправильный номер карты: %s", card_number)
         raise ValueError("Неправильный номер карты")
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+
+    masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    return masked_number
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция маскировки номера счета"""
+    masks_logger.info("Запуск функции get_mask_account.")
     if len(str(account_number)) != 20:
+        masks_logger.error("Неправильный номер счета: %s", account_number)
         raise ValueError("Неправильный номер счета")
-    return f"**{account_number[-4:]}"
+    masked_account = f"** {account_number[-4:]}"
+    return masked_account
 
 
-print(get_mask_card_number("7000792289606361"))
-print(get_mask_account("22425621641834121234"))
+try:
+    masks_logger.info(f"Замаскированный номер карты: {get_mask_card_number("7000792289606361")}")
+    masks_logger.info(f"Замаскированный номер счета: {get_mask_account("22425621641834121234")}")
+except ValueError as e:
+    masks_logger.exception("Ошибка: %s", e)
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,27 @@
 import json
+import logging
 import os
 
-import requests
+path = os.path.join(os.path.dirname(__file__), "..", "logs", "utils.log")
+file_path = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
+
+
+def setup_utils_logger() -> logging.Logger:
+    utils_logger = logging.getLogger("utils")
+    utils_logger.setLevel(logging.DEBUG)
+    utils_handler = logging.FileHandler(path, mode="w", encoding="utf-8")
+    utils_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+    utils_logger.addHandler(utils_handler)
+    return utils_logger
+
+
+utils_logger = setup_utils_logger()
 
 
 def loadtrfrom_json(file_path: str) -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
     try:
+        utils_logger.info("Запуск функции loadtrfrom_json.")
         with open(file_path, encoding="utf-8") as file:
             try:
                 content = json.load(file)
@@ -19,14 +34,15 @@ def loadtrfrom_json(file_path: str) -> list:
     return content
 
 
-file_path = (os.path.join(os.path.dirname(__file__), "..", "data", "operations.json"))
-print(loadtrfrom_json(file_path))
+utils_logger.info(loadtrfrom_json(file_path))
 content = loadtrfrom_json(file_path)
 
 
-def sum_transaction(money: dict) -> float | str:
+def sum_transaction(money: dict) -> float:
+    utils_logger.info("Запуск функции sum_transaction")
     """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
     from src.external_api import currency_conversion
+
     if money["operationAmount"]["currency"]["code"] == "RUB":
         result = money["operationAmount"]["amount"]
         return float(result)
@@ -35,8 +51,8 @@ def sum_transaction(money: dict) -> float | str:
 
 
 for transaction in content:
-    rub_amount = sum_transaction(transaction)
-    if rub_amount is not None:
-        print(f"Транзакция ID {transaction.get('id', 'неизвестный ID')}: Сумма в RUB = {rub_amount}")
+    amount = sum_transaction(transaction)
+    if amount is not None:
+        utils_logger.info(f"Транзакция ID {transaction.get('id', 'неизвестный ID')}: Сумма в RUB = {amount}")
     else:
-        print(f"Транзакция ID {transaction.get('id', 'неизвестный ID')} не в RUB или данные некорректны.")
+        utils_logger.info(f"Транзакция ID {transaction.get('id', 'неизвестный ID')} не в RUB или данные некорректны.")
