4df39d14182f4fedde904928c23275ac11ad98a3
Dmitriy <dm.tsiganov@gmail.com>
2025-01-04 21:05:09 +0700

    csv

---
+++ src/csvread.py
@@ -0,0 +1,16 @@
+import csv
+import os
+
+path = os.path.join(os.path.dirname(__file__), "..", "data", "transactions.csv")
+
+
+def read_csv(path: str) -> list:
+    rows = []
+    with open(path, mode="r", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter=";")
+        for row in reader:
+            rows.append(row)
+        return rows
+
+
+print(read_csv(path))
