0d3ed9f40ca2f5fb3def71761965e5683fd1b025
Dmitriy <dm.tsiganov@gmail.com>
2025-01-04 21:23:09 +0700

    csv1

--- src/csvread.py
+++ src/csvread.py
@@ -7,10 +7,10 @@ path = os.path.join(os.path.dirname(__file__), "..", "data", "transactions.csv")
 def read_csv(path: str) -> list:
     rows = []
     with open(path, mode="r", encoding="utf-8") as file:
-        reader = csv.reader(file, delimiter=";")
+        reader = csv.DictReader(file)
         for row in reader:
             rows.append(row)
-        return rows
+    return rows
 
 
 print(read_csv(path))
